984e2a8eae9f3c9ae603518c54c21365023e063c
Patrick Curl <patrick.curl@va.gov>
2020-05-26 10:25:51 -0500

    adding files to automate process for ocr containers

---
+++ scripts/OCR-loop-test.ps1
@@ -0,0 +1,45 @@
+﻿Set-Location /OCR
+# loop
+# grab all tif's
+# check to see if tif has pdf & text else make what is missing
+$i=1
+Do {
+
+  foreach ($tif in get-ChildItem -Recurse *.tif) {
+    Echo $tif.name
+    $dir = Split-Path -Path $tif -Resolve -Parent
+    $name = $dir+"/"+[System.IO.Path]::GetFileNameWithoutExtension("$tif")
+    $namePDF = $name + ".pdf"
+    $nameTXT = $name + ".txt"
+
+    if (Test-Path $namePDF -PathType leaf) 
+    {"File does Exist"
+    $pdf = 0}
+    else
+    {"File does not exist"
+    $pdf = 1
+    }
+
+    if (Test-Path $nameTXT -PathType leaf) 
+    {"File does Exist"
+    $txt = 0}
+    else
+    {"File does not exist"
+    $txt = 1
+    }
+
+    if ($pdf -eq '1' -AND $txt -eq '1') {
+    tesseract $tif $name -l eng --psm 4 txt pdf
+    }
+
+    if ($pdf -eq '0' -AND $txt -eq '1') {
+    tesseract $tif $name -l eng --psm 4 txt
+    }
+
+    if ($pdf -eq '1' -AND $txt -eq '0') {
+    tesseract $tif $name -l eng --psm 4 pdf
+    }
+}
+}
+While ($i -le 10)
+ 
